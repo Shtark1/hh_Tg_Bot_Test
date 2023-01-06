9863dbd369a823ef203239f89ce5248a9818e4ed
sergg <95623529+Shtark1@users.noreply.github.com>
2023-01-06 22:32:44 +0300

    version 1

--- cfg/cfg.py
+++ cfg/cfg.py
@@ -1,3 +1,1 @@
-TOKEN = '5620966011:AAHid8tjAkm87nvRsQ4KsohonKv-7gRRkrQ'
-# YOOPAYMENT = '1842663557:TEST:e7e356333bd787e98ce23ef9e1ed8bbc11938d37'
-# BOT_NICKNAME = "registration_of_visas_BOT"
+TOKEN = ''
